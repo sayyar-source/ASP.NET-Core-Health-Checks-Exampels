c0e27eb29bee1dca0cbc3c0520a3d85aee9d5e69
eng.sayyar <eng.sayyar@gmail.com>
2020-05-08 01:17:13 +0300

    Add project files.

---
+++ HealthCheckexample/Health/HealthCheckEX.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheckexample.Health
+{
+    public class HealthCheckEX : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+
+            var health = true;
+            if(health)
+            {
+                return Task.FromResult(
+                                HealthCheckResult.Healthy("healthed"));
+            }
+            else
+            {
+                return Task.FromResult(
+                               HealthCheckResult.Unhealthy("Unhealthy"));
+            }
+        }
+    }
+}
---
+++ HealthCheckexample/Health/TestHealthCheckWithArgs.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheckexample.Health
+{
+    public class TestHealthCheckWithArgs : IHealthCheck
+    {
+        public TestHealthCheckWithArgs(int a,string s,bool b)
+        {
+            A = a;
+            S = s;
+            B = b;
+        }
+        public int A { get; set; }
+        public string S { get; set; }
+        public bool B { get; set; }
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+           if(A>10 && S!=string.Empty && B==true)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("Healthy"));
+
+            }
+            return Task.FromResult(HealthCheckResult.Unhealthy("Unhealthy"));
+        }
+    }
+}
